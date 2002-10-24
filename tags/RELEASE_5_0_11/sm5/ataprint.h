ca0a5c0a866b7f72ddd2c5a30f2254e4d3af051e
nobody <nobody@4ea69e1a-61f1-4043-bf83-b5c94c648137>
2002-10-24 10:56:11 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_5_0_11'.

---
+++ tags/RELEASE_5_0_11/sm5/ataprint.h
@@ -0,0 +1,60 @@
+/*
+ * ataprint.h
+ *
+ * Home page of code is: http://smartmontools.sourceforge.net
+ *
+ * Copyright (C) 2002 Bruce Allen <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 1999-2000 Michael Cornwell <cornwell@acm.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * (for example COPYING); if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This code was originally developed as a Senior Thesis by Michael Cornwell
+ * at the Concurrent Systems Laboratory (now part of the Storage Systems
+ * Research Center), Jack Baskin School of Engineering, University of
+ * California, Santa Cruz. http://ssrc.soe.ucsc.edu/
+ *
+ */
+
+#ifndef _SMART_PRINT_H_
+#define _SMART_PRINT_H_
+
+#ifndef CVSID2
+#define CVSID2 "$Id: ataprint.h,v 1.12 2002/10/24 09:54:02 ballen4705 Exp $\n"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "atacmds.h"
+
+
+/* Prints ATA Drive Information and S.M.A.R.T. Capability */
+void ataPrintDriveInfo (struct hd_driveid);
+
+void ataPrintGeneralSmartValues (struct ata_smart_values);
+
+void ataPrintSmartThresholds (struct ata_smart_thresholds);
+
+void ataPrintSmartErrorlog (struct ata_smart_errorlog);
+
+void PrintSmartAttributes (struct ata_smart_values data);
+
+void PrintSmartAttribWithThres (struct ata_smart_values data,
+                                struct ata_smart_thresholds thresholds,
+				int onlyfailed);
+
+// returns number of entries that had logged errors
+int ataPrintSmartSelfTestlog(struct ata_smart_selftestlog data, int allentries);
+
+void ataPseudoCheckSmart (struct ata_smart_values , 
+                          struct ata_smart_thresholds );
+
+int ataPrintMain ( int fd );
+
+#endif
---
+++ tags/RELEASE_5_0_11/sm5/extern.h
@@ -0,0 +1,56 @@
+/*
+ * extern.h
+ *
+ * Home page of code is: http://smartmontools.sourceforge.net
+ *
+ * Copyright (C) 2002 Bruce Allen <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 1999-2000 Michael Cornwell <cornwell@acm.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * (for example COPYING); if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This code was originally developed as a Senior Thesis by Michael Cornwell
+ * at the Concurrent Systems Laboratory (now part of the Storage Systems
+ * Research Center), Jack Baskin School of Engineering, University of
+ * California, Santa Cruz. http://ssrc.soe.ucsc.edu/
+ *
+ */
+
+#ifndef _EXTERN_H_
+#define _EXTERN_H_
+
+
+#ifndef CVSID3
+#define CVSID3 "$Id: extern.h,v 1.8 2002/10/23 12:24:24 ballen4705 Exp $\n"
+#endif
+
+extern unsigned char driveinfo;
+extern unsigned char checksmart;
+extern unsigned char smartvendorattrib;
+extern unsigned char generalsmartvalues;
+extern unsigned char smartselftestlog;
+extern unsigned char smarterrorlog;
+extern unsigned char smartdisable;
+extern unsigned char smartenable; 
+extern unsigned char smartstatus;
+extern unsigned char smartexeoffimmediate;
+extern unsigned char smartshortselftest;
+extern unsigned char smartextendselftest;
+extern unsigned char smartshortcapselftest;
+extern unsigned char smartextendcapselftest;
+extern unsigned char smartselftestabort;
+extern unsigned char smartautoofflineenable;
+extern unsigned char smartautoofflinedisable;
+extern unsigned char smartautosaveenable;
+extern unsigned char smartautosavedisable;
+extern unsigned char smart009minutes;
+extern int           testcase;
+extern unsigned char quietmode;
+extern unsigned char veryquietmode;
+#endif

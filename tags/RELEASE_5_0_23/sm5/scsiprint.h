78b5b6ee856eee17379ec5f78c5c6b3f685d0d18
nobody <nobody@4ea69e1a-61f1-4043-bf83-b5c94c648137>
2002-10-30 06:02:41 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_5_0_23'.

---
+++ tags/RELEASE_5_0_23/sm5/scsiprint.h
@@ -0,0 +1,36 @@
+/*
+ * scsiprint.h
+ *
+ * Home page of code is: http://smartmontools.sourceforge.net
+ *
+ * Copyright (C) 2002 Bruce Allen <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 2000 Michael Cornwell <cornwell@acm.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * (for example COPYING); if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This code was originally developed as a Senior Thesis by Michael Cornwell
+ * at the Concurrent Systems Laboratory (now part of the Storage Systems
+ * Research Center), Jack Baskin School of Engineering, University of
+ * California, Santa Cruz. http://ssrc.soe.ucsc.edu/
+ *
+ */
+
+
+/* scsismart version number */
+#ifndef SCSI_PRINT_H_
+#define SCSI_PRINT_H_
+
+#ifndef CVSID5
+#define CVSID5 "$Id: scsiprint.h,v 1.7 2002/10/28 23:46:59 ballen4705 Exp $\n"
+#endif
+
+void scsiPrintMain(int fd);
+
+#endif
